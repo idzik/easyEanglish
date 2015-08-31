2e2f8a0fb8fcae8c43188dfb7a8f97fed5dccd7f
idzik <idzik235@gmail.com>
2015-08-31 22:09:25 +0200

    dodanie dzwieku mp3 z biblioteki JLayer

--- EasyEanglish/src/en/verify/VerifyWords.java
+++ EasyEanglish/src/en/verify/VerifyWords.java
@@ -1,14 +1,22 @@
 package en.verify;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
 
 public class VerifyWords {
 	
 	private int re = 0;
 	Map<String, String> badAnswer ;
+	FileInputStream fileInput = null;
+	Player player = null;
 
+	
 	public String checkAnswer(Map<String, String> mapWords){
 		re++;
 		
@@ -20,10 +28,24 @@ public class VerifyWords {
 			System.out.print(key + " Podaj odpowiedź  ");
 			String ans = sc.nextLine();
 			String goodAnswer = mapWords.get(key);
-			if(goodAnswer.equals(ans) != true){	
-				System.out.println("Prawidłowa odpowiedz to : " + goodAnswer);
-				badAnswer.put(key, goodAnswer);				
+			try {
+				fileInput = new FileInputStream("patient.mp3");
+				
+				if(goodAnswer.equals(ans) != true){	
+					System.out.println("Prawidłowa odpowiedz to : " + goodAnswer);
+					badAnswer.put(key, goodAnswer);			
+					player = new Player(fileInput);
+					player.play();
+				}
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (JavaLayerException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
+			
+		
 		}
 		
 		if(badAnswer != null && badAnswer.size()>0){
